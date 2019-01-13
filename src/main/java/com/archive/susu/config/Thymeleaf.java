2d8ca94bd7d5ec7a54e1cbe7dc1bfd604823b3e6
Bảo Lộc Tống <loctong@gmail.com>
2019-01-13 14:54:18 +0700

    [Config] Cac add resources ben ngoai vo du an

--- src/main/java/com/archive/susu/config/Thymeleaf.java
+++ src/main/java/com/archive/susu/config/Thymeleaf.java
@@ -88,9 +88,16 @@ public class Thymeleaf implements WebMvcConfigurer, ApplicationContextAware {
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/images/**").addResourceLocations("/assets/images/");
-        registry.addResourceHandler("/assets/css/**").addResourceLocations("/assets/css/");
-        registry.addResourceHandler("/assets/js/**").addResourceLocations("/assets/js/");
+        /**
+         * TODO: Cach su dung
+         * href="../../css/stsm.css" (vi tri dang dung cua html ra root roi ap dung mapping ben duoi)
+         * con dung tag th cua template vo thang luon ko can mapping nay
+         * th:href="@{/assets/css/stsm.css}"
+         */
+        registry.addResourceHandler("/assets/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/assets/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/assets/js/**").addResourceLocations("/js/");
+
     }
 
     /**
